24282f6a4638fbb5412263e0e9e1aaa6cd3c42fd
JasonDeras <derasjason2@gmail.com>
2019-01-25 13:36:07 -0600

    Agregando el menu

--- lab#2_JasonDeras/src/lab/pkg2_jasonderas/Lab2_JasonDeras.java
+++ lab#2_JasonDeras/src/lab/pkg2_jasonderas/Lab2_JasonDeras.java
@@ -1,21 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lab.pkg2_jasonderas;
 
-/**
- *
- * @author jason
- */
 public class Lab2_JasonDeras {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
     }
-    
+
+    static int menu() {
+        System.out.println("1- Registro de casas");
+        System.out.println("2- Manejo de estados");
+    }
 }
