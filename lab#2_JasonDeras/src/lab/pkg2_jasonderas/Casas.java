d914ea95437c9bf6a85fe6a24b794fd166905f8a
JasonDeras <derasjason2@gmail.com>
2019-01-25 14:33:39 -0600

    Sigo creando la clase de casas

--- lab#2_JasonDeras/src/lab/pkg2_jasonderas/Casas.java
+++ lab#2_JasonDeras/src/lab/pkg2_jasonderas/Casas.java
@@ -1,5 +1,23 @@
 package lab.pkg2_jasonderas;
 
+import java.awt.Color;
+
 public class Casas {
 
-}
+    int num_casa;
+    int num_bloque;
+    Color c = Color.white;
+    int ancho;
+    int largo;
+    String estado;
+    int pisos;
+    int baños;
+    int cuartos;
+    String dueño;
+
+    public Casas() {
+    }//Constructor vacio
+
+    public Casas(int n_c, int n_b, Color c, int ancho, int largo, String estado, int pisos, int baños, int cuartos, String dueño) {
+    }//Constructro con los parametros
+}//Fin de la clase de las casas
